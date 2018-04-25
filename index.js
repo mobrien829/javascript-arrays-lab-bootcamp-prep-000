24b8b4caf303bef85646aec1b354d0c6aed0f317
Michael O%27Brien <mobrien@colgate.edu>
2018-04-25 17:40:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ function destructivelyAppendKitten(name) {
   kittens.push(name)
   return kittens
 }
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
